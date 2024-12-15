b2b56622b471c2cec65a9f16a90468f99e3c15a4
Ryan <ryanrigor4@gmail.com>
2024-12-15 14:14:34 -0800

    init testing

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/app/(.*)$": "<rootDir>/app/$1",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
+
+export default config;
